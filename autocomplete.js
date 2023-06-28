8ef9b9f5f67477cc40f8762eb2ba33793ee7243a
Kevinkilote <117811023+Kevinkilote@users.noreply.github.com>
2023-06-28 09:19:48 +0900

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,35 @@
+let availableKeywords = [
+    'Starbucks coffee',
+    'Hoshino coffee',
+    'TULLY coffee',
+    'Ogawa coffee',
+    'Arabica %',
+    'PRONTO',
+    'Inoda coffee',
+    'Perk coffee',
+    'Saryo FUKUCHA',
+    'Nakamura Tokichi',
+    
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+}
+function display(result){
+    const content = result.map((list)=>{
+        return "<li>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
